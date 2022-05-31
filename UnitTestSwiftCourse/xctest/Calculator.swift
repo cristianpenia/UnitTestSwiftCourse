5b3a2bfec6867f367ac549590b8bc2039a426396
Cristian_Mac2015 <cris_pb@protonmail.com>
2022-05-30 20:20:27 -0500

    [ADD] se agrego la clase Calculator

---
+++ UnitTestSwiftCourse/xctest/Calculator.swift
@@ -0,0 +1,19 @@
+//
+//  Calculator.swift
+//  UnitTestSwiftCourse
+//
+//  Created by Cristian Peña Barrios on 30/05/22.
+//
+
+import Foundation
+
+
+class Calculator {
+    func add(n1: Int, n2: Int) -> Int {
+        return n1 + n2
+    }
+    
+    func substract(n1: Int, n2: Int) -> Int {
+        return n1 - n2
+    }
+}
